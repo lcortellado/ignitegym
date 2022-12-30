b9ceb72c2870e062282aeb7ef5c5a558357bdfbb
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2022-12-30 17:12:25 -0300

    Added statusBar

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { StatusBar, Text, View } from 'react-native';
 import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 export default function App() {
@@ -8,7 +8,12 @@ export default function App() {
   })
 
   return (
-    <View>
+    <View style ={{flex: 1, justifyContent:'center', alignItems: 'center', backgroundColor: 'gray'}}>
+    <StatusBar 
+    barStyle="light-content"
+    backgroundColor="transparent"
+    translucent
+    />
     {fontsLoaded ?  <Text>Open up App.tsx to start working on your app!</Text> : <View />} 
     </View>
   );
