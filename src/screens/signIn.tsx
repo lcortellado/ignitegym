d8da7400ebc28b88b905bd5db6800d9d2a871d9e
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2023-01-03 20:46:07 -0300

    Added config for svg

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,6 +1,7 @@
-import { Image, VStack } from "native-base";
+import { Center, Image, Text, VStack } from "native-base";
 
 import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg'
 
 export function SignIn() {
     return(
@@ -11,6 +12,15 @@ export function SignIn() {
           resizeMode="contain"
           position="absolute"
           />
+          <Center my={24}>
+          <LogoSvg />
+          <Text color="gray.100" fontSize="sm">
+          Entrene su mente y su cuerpo
+          </Text>
+          
+          </Center>
+         
+          
         </VStack>
     )
 }
