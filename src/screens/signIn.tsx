637c8c573027b106f34ccad7e7e9257d5d08d59e
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2023-01-03 20:56:37 -0300

    Added heading

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,26 +1,29 @@
-import { Center, Image, Text, VStack } from "native-base";
+import { Center, Heading, Image, Text, VStack } from "native-base";
 
-import BackgroundImg from '@assets/background.png'
-import LogoSvg from '@assets/logo.svg'
+import BackgroundImg from "@assets/background.png";
+import LogoSvg from "@assets/logo.svg";
 
 export function SignIn() {
-    return(
-        <VStack flex={1} bg="gray.700">
-          <Image 
-          source={BackgroundImg}
-          alt="Personas entrenando"
-          resizeMode="contain"
-          position="absolute"
-          />
-          <Center my={24}>
-          <LogoSvg />
-          <Text color="gray.100" fontSize="sm">
+  return (
+    <VStack flex={1} bg="gray.700">
+      <Image
+        source={BackgroundImg}
+        alt="Personas entrenando"
+        resizeMode="contain"
+        position="absolute"
+      />
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">
           Entrene su mente y su cuerpo
-          </Text>
-          
-          </Center>
-         
-          
-        </VStack>
-    )
+        </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+          Acceda a su cuenta
+        </Heading>
+      </Center>
+    </VStack>
+  );
 }
