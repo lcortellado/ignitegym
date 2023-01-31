bffd9ce04a0d7da61e697bf81fc3337c1852f85e
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2023-01-30 22:40:18 -0300

    Created homeHeader component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,29 @@
+import { Heading, HStack, Text, VStack, Icon } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
+import { UserPhoto } from "./UserPhoto";
+import { TouchableOpacity } from "react-native";
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} alignItems="center">
+      <UserPhoto
+        size={16}
+        source={{ uri: "https://github.com/lcortellado.png" }}
+        alt="Imagen de usuario"
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Hola,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Carlos
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
+}
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import { HomeHeader } from "@components/HomeHeader";
+import { VStack, Text } from "native-base";
+
+export function Home() {
+  return (
+    <VStack flex={1}>
+      <HomeHeader />
+    </VStack>
+  );
+}
