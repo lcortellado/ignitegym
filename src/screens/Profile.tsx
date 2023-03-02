97e5eea744e716931e7a026854262a23446aefa3
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2023-03-01 22:46:06 -0300

    Added expo-fileSystem

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,3 +1,4 @@
+import { TouchableOpacity } from "react-native";
 import {
   Center,
   ScrollView,
@@ -5,11 +6,15 @@ import {
   Skeleton,
   Text,
   Heading,
+  useToast,
 } from "native-base";
+import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system";
+
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
 import { useState } from "react";
-import { TouchableOpacity } from "react-native";
+
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 
@@ -17,6 +22,55 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState(
+    "https:github.com/lcortellado.png"
+  );
+
+  const toast = useToast();
+
+  async function handleUserPhotoSelect() {
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      console.log(photoSelected.assets[0].uri, "urifoto");
+      if (photoSelected.assets[0].uri) {
+        const photo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        /* It's converting the size of the photo from bytes to
+        megabytes. 
+        photo.size / 1024 / 1024
+        */
+        if (photo.size && photo.size / 1024 / 1024 > 5) {
+          return toast.show({
+            title:
+              "Esta imagen es demasiado grande. Escoja otro hasta un maximo de 5mb",
+            placement: "top",
+            bgColor: "red.500",
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setPhotoIsLoading(false);
+    }
+  }
+
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
@@ -33,12 +87,12 @@ export function Profile() {
             />
           ) : (
             <UserPhoto
-              source={{ uri: "https:github.com/lcortellado.png" }}
+              source={{ uri: userPhoto }}
               alt="Foto de usuario"
               size={PHOTO_SIZE}
             />
           )}
-          <TouchableOpacity>
+          <TouchableOpacity onPress={handleUserPhotoSelect}>
             <Text
               color="green.500"
               fontWeight="bold"
